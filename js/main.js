b7f0bea612cba2e38161b8c2a18730666240e6c9
Roman Schigolev <r.shchigolev2018@yandex.ru>
2019-07-14 00:15:46 +0700

    Added a pop-up window under "Open Map"

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Accordion
 const accordion = document.querySelector('.service__accordion');
 const items = accordion.querySelectorAll('.accordion__item');
 const buttons = accordion.querySelectorAll('.accordion__header');
@@ -13,64 +14,77 @@ function toggleAccordion() {
         item.classList.remove('open');
     });
 }
-
 buttons.forEach(accordion__header => accordion__header.addEventListener('click', toggleAccordion));
 
+//PopUp Map
+const openModalButtons = document.querySelectorAll('[data-open-modal]');
+const closeModalButtons = document.querySelectorAll('[data-close-modal]');
+const overlay = document.querySelector('.modal__overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.openModal);
+        openModal(modal);
+    });
+});
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    });
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        closeModal(modal);
+    });
+});
+
+function openModal(modal) {
+    if (modal == null){
+        return;
+    }
+    modal.classList.add('active');
+    overlay.classList.add('active');
+    document.onkeydown = (event) => {
+        const modals = document.querySelectorAll('.modal.active');
+        modals.forEach(modal => {
+            if (event.key === "Escape"){
+                closeModal(modal);
+            }
+        });
+    }
+}
+
+function closeModal(modal) {
+    if (modal == null){
+        return;
+    }
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+    document.onkeydown = null;
+}
 
 
-// const carouselSlide = document.querySelector('.testimonials__list');
-// const carouselItems = document.querySelectorAll('.testimonials__item');
+// WORK IT
+// const menuSlide = () => {
+//     const burger = document.querySelector('.menu__burger');
+//     const menu = document.querySelector('.menu__list');
+//     const menuItem = document.querySelectorAll('.menu__item');
 //
-// //Buttons
-// const prevBtn = document.querySelector('.testimonials__btn--prev');
-// const nextBtn = document.querySelector('.testimonials__btn--next');
+//     burger.addEventListener('click', () =>{
+//         menu.classList.toggle('menu__active');
 //
-// //Counter
-// let counter = 1;
-// const size = carouselItems[0].clientWidth;
-//
-// carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-//
-// //Button Listener
-// nextBtn.addEventListener('click', () => {
-//     carouselSlide.style.transition="transform 0.3s ease";
-//     counter++;
-//     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-// });
-
-
-// let carouselItem = document.querySelectorAll('.testimonials__list .testimonials__item');
-// let current = 0;
-//
-// function slider(){
-//     for (let i = 0; i < carouselItem.length; i++){
-//         carouselItem[i].classList.add('opacityZero');
-//     }
-//     carouselItem[current].classList.remove('opacityZero');
-//     if(current+1 == carouselItem.length){
-//         current = 0;
-//     }
-//     else {
-//         current++;
-//     }
+//         menuItem.forEach((link, index) => {
+//             if (link.style.animation) {
+//                 link.style.animation = '';
+//             } else {
+//                 link.style.animation = `menuItemFade 0.5s ease forwards ${index / 3 + 0.3}s`;
+//             }
+//         });
+//     });
 // }
 //
-// // document.querySelector('.testimonials__list').onclick = slider;
-// document.querySelector('.testimonials__btn--prev').onclick = () =>{
-//     if (current - 1 == -1){
-//         current = carouselItem.length - 1;
-//     }
-//     else {
-//         current--;
-//     }
-//     slider();
-// };
-// document.querySelector('.testimonials__btn--next').onclick = () =>{
-//     if (current + 1 == carouselItem.length){
-//         current = 0;
-//     }
-//     else {
-//         current++;
-//     }
-//     slider();
-// };
+// menuSlide();
