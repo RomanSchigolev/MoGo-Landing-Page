2cc6f78f8088dc159e0afe1b82201febb805fa0c
Roman Schigolev <r.shchigolev2018@yandex.ru>
2019-10-23 21:11:19 +0700

    added jquery(links-anchors)

--- dist/libraries.js
+++ dist/libraries.js
@@ -1,1 +1,22 @@
 "use strict";
+
+$(document).ready(function () {
+  $('[data-scroll]').on('click', function (event) {
+    event.preventDefault();
+    var sectionId = $(this).data('scroll');
+    var sectionOffset = $(sectionId).offset().top;
+    $('html, body').animate({
+      scrollTop: sectionOffset
+    }, 1000);
+  }); // $('burger').on("click", function(event) {
+  //   event.preventDefault();
+  //   $(this).toggleClass('burger--active');
+  //   $('.menu__list').toggleClass('.menu__list--active').show();
+  // });
+  // $('[data-scroll]').on('click', function(event){
+  //   event.preventDefault();
+  //   $('burger').removeClass('burger--active');
+  //   $('.menu__list').hide();
+  //   $('burger').removeClass('active');
+  // });
+});
---
+++ js/libraries.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('[data-scroll]').on('click', function(event) {
+    event.preventDefault();
+    const sectionId = $(this).data('scroll');
+    const sectionOffset = $(sectionId).offset().top;
+
+    $('html, body').animate({scrollTop: sectionOffset}, 1000);
+  });
+  // $('burger').on("click", function(event) {
+  //   event.preventDefault();
+  //   $(this).toggleClass('burger--active');
+  //   $('.menu__list').toggleClass('.menu__list--active').show();
+  // });
+
+  // $('[data-scroll]').on('click', function(event){
+  //   event.preventDefault();
+  //   $('burger').removeClass('burger--active');
+  //   $('.menu__list').hide();
+  //   $('burger').removeClass('active');
+  // });
+});
